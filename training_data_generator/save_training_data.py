630a30880a2c610568f73e1c1d27460b6201119b
Julian C <98852100+N4T10N01@users.noreply.github.com>
2023-03-25 18:45:11 -0400

    Add files via upload

---
+++ training_data_generator/save_training_data.py
@@ -0,0 +1,28 @@
+import cv2
+import sys
+from hand_coded_lane_follower import HandCodedLaneFollower
+
+
+def save_image_and_steering_angle(video_file):
+    lane_follower = HandCodedLaneFollower()
+    cap = cv2.VideoCapture(video_file + '.avi')
+
+    try:
+        i = 0
+        while cap.isOpened():
+            _, frame = cap.read()
+            lane_follower.follow_lane(frame)
+            cv2.imwrite("%s_%03d_%03d.png" % (video_file, i, lane_follower.curr_steering_angle), frame)
+            i += 1
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except Exception as e:
+        print(e.message)
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    path="/home/pi/DeepPiCar/models/lane_navigation/data/images/video4"
+    save_image_and_steering_angle(path)
